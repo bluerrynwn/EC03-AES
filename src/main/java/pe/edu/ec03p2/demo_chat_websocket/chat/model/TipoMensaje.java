60aa4b753d4a9c07dd836c2cb54007cfd9de1c88
bluerrynwn <bluerrynwn@gmail.com>
2025-03-16 15:34:36 -0500

    segundo commit

---
+++ src/main/java/pe/edu/ec03p2/demo_chat_websocket/chat/model/TipoMensaje.java
@@ -0,0 +1,4 @@
+package pe.edu.ec03p2.demo_chat_websocket.chat.model;
+
+public class TipoMensaje {
+}
---
+++ src/main/java/pe/edu/ec03p2/demo_chat_websocket/config/WebSocketConfig.java
@@ -0,0 +1,4 @@
+package pe.edu.ec03p2.demo_chat_websocket.config;
+
+public class WebSocketConfig {
+}
