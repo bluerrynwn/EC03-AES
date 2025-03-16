22dd5a02183cfc532fd9cc50bd1d090091a6baee
bluerrynwn <bluerrynwn@gmail.com>
2025-03-16 15:18:53 -0500

    primer commit

---
+++ src/main/java/pe/edu/ec03p2/demo_chat_websocket/DemoChatWebsocketApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.ec03p2.demo_chat_websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoChatWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoChatWebsocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/ec03p2/demo_chat_websocket/DemoChatWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.ec03p2.demo_chat_websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoChatWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
